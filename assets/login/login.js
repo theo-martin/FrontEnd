ca53a3450d7e1937544361eb02c46855d5b27bf0
theo-martin <theomartin1999@gmail.com>
2024-01-23 23:56:59 +0100

    resolution d'une partie du probleme du login

--- assets/login/login.js
+++ assets/login/login.js
@@ -4,33 +4,51 @@ const sectionLogin = document.querySelector("#login")
 
 form.addEventListener("submit", async (event)=>{
     event.preventDefault()
-    const userLogin = {
-        email: event.target.querySelector("[name=email]").value.trim,
-        password: event.target.querySelector("[name=password]").value.trim,
-    }
-    const userBodyValue = JSON.stringify(userLogin)
-    fetch("http://localhost:5678/api/users/login", {
+    
+        const useremail = event.target.querySelector("[name=email]").value.trim
+        const userpassword =  event.target.querySelector("[name=password]").value.trim
+        
+    // const userLogin = {userpassword, useremail}
+  //   const userLogin = {
+  //     email: event.target.querySelector("[name=email]").value.trim,
+  //     password: event.target.querySelector("[name=password]").value.trim,
+  // }
+    const userBodyValue = JSON.stringify(useremail, userpassword)
+    
+    fetch("http://localhost:5678/api/users/login",{
         method: "POST",
         headers: { "Content-Type": "application/json",
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST',
         'Access-Control-Allow-Headers': 'Content-Type' },
+        // body: JSON.stringify(useremail, userpassword),
         body: userBodyValue,
       })
+    // fetch('http://localhost:5678/api/users/login', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+    //   body: JSON.stringify(useremail, userpassword),
+		// 	// body: JSON.stringify(userLogin),
+		// }) 
+    
       .then(res => {
       if (res.status === 200) {
-        
+        console.log(useremail, userpassword)
           return res.json()
+          
       }
         // La connexion a réussi
        else {
+        
         throw new Error("la connexion à échoué veuillez verifier l'identifiant ou le mots de passe")
         // La connexion a échoué
       }
     })
-    .then(data => {
+   .then(data => {
     console.log(data.token)
-    localStorage.setItem("token", data.token)
+    localStorage.setItem(`token`, data.token)
     location.href="../../index.html"
     })
     .catch(error => {
@@ -40,6 +58,7 @@ form.addEventListener("submit", async (event)=>{
 })
 
 function messageErreur(message){
+   
     const divErreur = document.createElement("div")
     divErreur.classList.add("erreur")
     const messageErreurElement = document.createElement("p")
